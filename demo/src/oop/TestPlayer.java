eb0bda28534e6bd3c9d11c5e9b5c1ca397d091ce
srikanthpragada <srikanthpragada@gmail.com>
2021-09-23 18:56:11 +0530

    23-SEP

--- demo/src/oop/TestPlayer.java
+++ demo/src/oop/TestPlayer.java
@@ -1,68 +1,72 @@
 package oop;
 
-class Player {
+abstract class Player {
 	private String name;
 
 	public Player(String name) {
 		super();
 		this.name = name;
 	}
+
 	public void print() {
-        System.out.println(this.name);
+		System.out.println(this.name);
 	}
-    public int getPoints() {
-    	return 0;
-    }
+
+	public abstract int getPoints();
+		
 }
 
 class Cricketer extends Player {
-	  private int runs;
-	  public Cricketer(String name, int runs) {
-		  super(name);
-		  this.runs = runs;
-	  }
-	  @Override 
-	  public void print() {
-		  super.print();
-		  System.out.println(this.runs);
-	  }
-	  
-	  public int getPoints() {
-		  return  this.runs / 100;
-	  }
+	private int runs;
+
+	public Cricketer(String name, int runs) {
+		super(name);
+		this.runs = runs;
+	}
+
+	@Override
+	public void print() {
+		super.print();
+		System.out.println(this.runs);
+	}
+
+	public int getPoints() {
+		return this.runs / 100;
+	}
 }
 
 class Footballer extends Player {
-	  private int matches;
-	  public Footballer(String name, int matches) {
-		  super(name);
-		  this.matches = matches;
-	  }
-	  @Override 
-	  public void print() {
-		  super.print();
-		  System.out.println(this.matches);
-	  }
-	  
-	  public int getPoints() {
-		  return this.matches / 2;
-	  }
-}
+	private int matches;
 
+	public Footballer(String name, int matches) {
+		super(name);
+		this.matches = matches;
+	}
+
+	@Override
+	public void print() {
+		super.print();
+		System.out.println(this.matches);
+	}
+
+	public int getPoints() {
+		return this.matches / 2;
+	}
+}
 
 public class TestPlayer {
 
 	public static void main(String[] args) {
-		// Compile-time polymorphism 
+		// Compile-time polymorphism
 		System.out.println(10);
 		System.out.println("Abc");
 		
-		Player p = new Cricketer("Abc",3939);  // Upcasting
-		p.print();  //  Runtime polymorphism
+		Player p = new Cricketer("Abc", 3939); // Upcasting
+		p.print(); // Runtime polymorphism
 		System.out.println(p.getPoints());
-		
-		p = new Footballer("Pqr",110);
-		p.print(); //  Runtime polymorphism
+
+		p = new Footballer("Pqr", 110);
+		p.print(); // Runtime polymorphism
 		System.out.println(p.getPoints());
 	}
 
---
+++ demo/src/oop/TestSealed.java
@@ -0,0 +1,21 @@
+package oop;
+
+//sealed class A permits B, C {
+//	
+//}
+//
+//class B extends A {
+//	
+//}
+//
+//class C extends A {
+//	
+//}
+public class TestSealed {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
